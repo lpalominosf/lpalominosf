faa56185b9bd3bd90a5b134627546dd9ef6a0376
Lore Palominos <lorena.palominos@gmail.com>
2018-03-19 22:54:45 -0300

    correccion

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,10 @@ $(document).ready(function(){
   // Initialize collapse button
   $(".button-collapse").sideNav();
 });
+
+  $(".foo, .bar").smoove({
+    offset: '15%',
+    // moveX is overridden to -200px for ".bar" object
+    moveX: '100px',
+    moveY: '100px',
+  });
